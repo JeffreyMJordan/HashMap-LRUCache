a40c9d921a9847690ab3370b49b2e520a2777b5d
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-30 15:39:44 -0800

    Completes practical

--- lib/p07_practical.rb
+++ lib/p07_practical.rb
@@ -1,5 +1,23 @@
 require_relative 'p05_hash_map'
 
 def can_string_be_palindrome?(string) 
-  
+  hash = HashMap.new
+  string.chars.each do |char| 
+    if hash.include?(char)
+      hash.delete(char)
+    else 
+      hash[char] = true
+    end 
+  end 
+  hash.num_keys <= 1  
 end
+
+class HashMap 
+  def num_keys 
+    res = 0 
+    self.each do |k,v|
+      res += 1
+    end
+    res 
+  end 
+end 
