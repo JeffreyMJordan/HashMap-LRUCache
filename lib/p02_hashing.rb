e9dde452ee8e09e4ea0fad4fde9aa535313bf330
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-12-30 13:08:38 -0800

    Completes hashing exercise

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    res = 0
+    self.dup.each_with_index do |item, idx| 
+      res += (item.hash * idx.hash)
+    end 
+    res 
   end
 end
 
 class String
   def hash
+    res = 0 
+    self.chars.each_with_index do |char, idx| 
+      res += (char.ord.hash * idx.hash)
+    end 
+    res 
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    res = 0 
+    self.each do |k, v| 
+      res += (k.hash/v.hash)
+    end 
+    res 
   end
 end
